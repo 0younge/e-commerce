660dc1bee9f4df92b81d7f805ca4a470d6c7b294
0younge <s45628439@gmail.com>
2026-04-24 00:50:50 +0900

    feat: 연관관계 설정
    - 연관관계를 기본으로 설정해두었습니다
    
    주석은 지우지 말 것!!

--- src/main/java/com/ecommerce/orders/entity/Order.java
+++ src/main/java/com/ecommerce/orders/entity/Order.java
@@ -1,11 +1,17 @@
 package com.ecommerce.orders.entity;
 
+import com.ecommerce.admins.entity.Admin;
 import com.ecommerce.common.BaseEntity;
+import com.ecommerce.products.entity.Product;
+import com.ecommerce.users.entity.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -20,5 +26,20 @@ public class Order extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long orderId;
+
+	// 위아래 사이 이 주석 밑에 필드값 추가하시면 됩니다!
+
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userId")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "productId")
+	private Product product;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "adminId")
+	private Admin admin;
 
 }
