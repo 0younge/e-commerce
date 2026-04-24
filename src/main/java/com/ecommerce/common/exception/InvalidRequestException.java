340da6a04094e563c77902d0ab1aa81312b39222
HS <ehehqp@gmail.com>
2026-04-24 15:38:27 +0900

    feat : 커스텀 예외 구현(데이터 없음, 권한 없음, 로그인 실패, 중복 데이터, 잘못된 요청)

--- src/main/java/com/ecommerce/common/exception/InvalidRequestException.java
+++ src/main/java/com/ecommerce/common/exception/InvalidRequestException.java
@@ -3,7 +3,7 @@ package com.ecommerce.common.exception;
 import org.springframework.http.HttpStatus;
 
 /**
- * 비즈니스 규칙 위반 (DTO 검증 말고, 로직 검증) 예시) 주문 수량은 1개 이상이어야 합니다.
+ * 비즈니스 규칙 위반 (DTO 검증 말고, 로직 검증) 예시) 주문 수량은 1개 이상이어야 합니다..
  */
 public class InvalidRequestException extends BusinessException {
 
