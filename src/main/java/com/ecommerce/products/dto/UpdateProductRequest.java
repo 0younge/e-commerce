04d729c6ae205d4328efd9888bcbaea83d3ce3e4
hwang67120 <hwang67120@gmail.com>
2026-04-24 16:01:00 +0900

    feat : UpdateProductRequest.java 추가

---
+++ src/main/java/com/ecommerce/products/dto/UpdateProductRequest.java
@@ -0,0 +1,18 @@
+package com.ecommerce.products.dto;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateProductRequest {
+
+	private  String name;
+
+	private  String category;
+
+	private BigDecimal price;
+
+}
