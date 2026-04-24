2ac912e20fa90937ff1bad39c0081317eac13340
hjh <hjh690@naver.com>
2026-04-24 12:45:50 +0900

    feat : 정적 팩토리 메서드 추가

--- src/main/java/com/ecommerce/users/dto/GetPageResponse.java
+++ src/main/java/com/ecommerce/users/dto/GetPageResponse.java
@@ -2,21 +2,27 @@ package com.ecommerce.users.dto;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import lombok.Getter;
 
 @Getter
-public class GetPageResponse {
-	private final List<GetUserResponse> content;
+public class GetPageResponse<T> {
+	private final List<T> content;
 	private final int page;
 	private final int size;
 	private final Long totalElements;
 	private final int totalPages;
 
-	public GetPageResponse(List<GetUserResponse> content, int page, int size, Long totalElements, int totalPages) {
-		this.content = content;
-		this.page = page;
-		this.size = size;
-		this.totalElements = totalElements;
-		this.totalPages = totalPages;
+	private GetPageResponse(Page<T> page) {
+		this.content = page.getContent();
+		this.page = page.getNumber() + 1;
+		this.size = page.getSize();
+		this.totalElements = page.getTotalElements();
+		this.totalPages = page.getTotalPages();
+	}
+
+	public static <T> GetPageResponse<T> of(Page<T> page) {
+		return new GetPageResponse<>(page);
 	}
 }
