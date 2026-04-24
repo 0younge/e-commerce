a8b7086ff921571154ff09127d6c26f770dcc4ef
hwang67120 <hwang67120@gmail.com>
2026-04-24 15:45:28 +0900

    feat : Product.java 추가

--- src/main/java/com/ecommerce/products/entity/Product.java
+++ src/main/java/com/ecommerce/products/entity/Product.java
@@ -1,7 +1,10 @@
 package com.ecommerce.products.entity;
 
+import java.math.BigDecimal;
+
 import com.ecommerce.common.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +22,22 @@ public class Product extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
 	private Long productId;
+
+	@Column(name = "admin_id", nullable = false)
+	private Long adminId;
+
+	@Column(nullable = false, length = 255)
+	private String name;
+
+	@Column(nullable = false, length = 50)
+	private String category;
+
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal price;
+
+	@Column(nullable = false)
+	private Integer quantity;
 
 }
