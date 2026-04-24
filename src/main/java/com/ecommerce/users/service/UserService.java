0b7ef10818a917cfd3c4ea0df46623f28627b7eb
hjh <hjh690@naver.com>
2026-04-24 13:12:20 +0900

    feat: 키워드 기반 유저 목록 조회 서비스 로직 추가

--- src/main/java/com/ecommerce/users/service/UserService.java
+++ src/main/java/com/ecommerce/users/service/UserService.java
@@ -1,7 +1,12 @@
 package com.ecommerce.users.service;
 
-import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ecommerce.users.dto.GetUserResponse;
 import com.ecommerce.users.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +17,16 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
+	@Transactional(readOnly = true)
+	public Page<GetUserResponse> findUserByKeyword(String keyword, Pageable pageable) {
+		return userRepository.findByKeyword(keyword, pageable)
+			.map(user -> new GetUserResponse(
+				user.getUserId(),
+				user.getName(),
+				user.getEmail(),
+				user.getPhoneNumber(),
+				user.getStatus().name(),
+				user.getCreatedAt()
+			));
+	}
 }
