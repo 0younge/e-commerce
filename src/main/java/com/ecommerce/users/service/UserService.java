bc90366a57750acdc2c57c0388ad5115dfafa54a
hjh <hjh690@naver.com>
2026-04-24 14:01:26 +0900

    feat : 고객 상세 조회 서비스 로직 추가

--- src/main/java/com/ecommerce/users/service/UserService.java
+++ src/main/java/com/ecommerce/users/service/UserService.java
@@ -1,12 +1,12 @@
 package com.ecommerce.users.service;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.users.dto.GetUserResponse;
+import com.ecommerce.users.entity.User;
 import com.ecommerce.users.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -28,5 +28,19 @@ public class UserService {
 				user.getStatus().name(),
 				user.getCreatedAt()
 			));
+	}
+
+	@Transactional(readOnly = true)
+	public GetUserResponse findUserDetails(Long userId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalStateException("해당 유저를 찾을 수 없습니다."));
+
+		return new GetUserResponse(
+			user.getUserId(),
+			user.getName(),
+			user.getEmail(),
+			user.getPhoneNumber(),
+			user.getStatus().name(),
+			user.getCreatedAt());
 	}
 }
