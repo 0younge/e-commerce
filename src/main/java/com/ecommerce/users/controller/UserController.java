1bb9fb6c14229c92eb9b651752cf5ccb6154eeee
hjh <hjh690@naver.com>
2026-04-24 14:00:43 +0900

    feat : 고객 상세 조회 API 추가

--- src/main/java/com/ecommerce/users/controller/UserController.java
+++ src/main/java/com/ecommerce/users/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +30,15 @@ public class UserController {
 		@RequestParam(required = false, defaultValue = "name") String sortBy,
 		@RequestParam(required = false, defaultValue = "ASC") String sortOrder,
 		@RequestParam String status
-		) {
+	) {
 		Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.valueOf(sortOrder), sortBy);
 		Page<GetUserResponse> result = userService.findUserByKeyword(keyword, pageable);
 		return ResponseEntity.status(HttpStatus.OK).body(GetPageResponse.of(result));
+	}
+
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<GetUserResponse> getUserDetails(@PathVariable Long userId) {
+		return ResponseEntity.status(HttpStatus.OK).body(userService.findUserDetails(userId));
 	}
 
 }
