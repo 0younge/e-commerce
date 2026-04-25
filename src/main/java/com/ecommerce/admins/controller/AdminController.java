d429a99e326581a4d1069cf5bf51d587576b7780
0younge <s45628439@gmail.com>
2026-04-25 16:20:07 +0900

    refactor: 오류 수정, 주석 추가
    - 기능에 상태에 따른 검증 추가
    - 오류 수정
    - 주석 추가

--- src/main/java/com/ecommerce/admins/controller/AdminController.java
+++ src/main/java/com/ecommerce/admins/controller/AdminController.java
@@ -38,6 +38,11 @@ public class AdminController {
 
 	private final AdminService adminService;
 
+	/**
+	 * 회원가입
+	 * @param request 회원가입 정보
+	 * @return 상태코드
+	 */
 	@PostMapping("/signup")
 	public ResponseEntity<Void> createAdmin(@RequestBody @Valid CreateAdminRequest request) {
 		adminService.save(request);
@@ -45,15 +50,33 @@ public class AdminController {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
+	/**
+	 * 로그인
+	 * @param request 로그인에 필요한 정보
+	 * @param session 검증을 위한 세션
+	 * @return 상태코드
+	 */
 	@PostMapping("/login")
 	public ResponseEntity<Void> loginAdmin(@RequestBody @Valid LoginAdminRequest request, HttpSession session) {
-		AdminInfo adminInfo = adminService.login(request, checkSessionOrThrow(session));
+		AdminInfo adminInfo = adminService.login(request);
 		session.setMaxInactiveInterval(86400); // 세션만료: 24시간
 		session.setAttribute(AdminConst.ADMIN_INFO, adminInfo);
 
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * 관리자 전체 조회
+	 * @param keyword 검색할 키워드
+	 * @param page 페이지 넘버
+	 * @param size 페이지 사이즈
+	 * @param sortBy 정렬기준
+	 * @param sortOrder 정렬 순서
+	 * @param role 검색할 역할
+	 * @param status 검색할 상태
+	 * @param session 검증을 위한 세션
+	 * @return 페이지네이션을 마친 관리자 리스트
+	 */
 	@GetMapping
 	public ResponseEntity<Page<GetAdminResponse>> getAdminList(@RequestParam(required = false) String keyword,
 		@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size,
@@ -67,11 +90,24 @@ public class AdminController {
 			adminService.getAdminList(keyword, role, status, pageable, checkSessionOrThrow(session)));
 	}
 
+	/**
+	 * 특정 관리자 조회
+	 * @param adminId 조회할 관리자 아이디
+	 * @param session 검증을 위한 세션
+	 * @return 특정 관리자의 이름, 메일, 전화번호, 역할, 상태, 생성일, 수락일 반환
+	 */
 	@GetMapping("/{adminId}")
 	public ResponseEntity<GetOneAdminResponse> getOneAdmin(@PathVariable Long adminId, HttpSession session) {
 		return ResponseEntity.ok(adminService.getOne(adminId, checkSessionOrThrow(session)));
 	}
 
+	/**
+	 * 관리자 정보 수정
+	 * @param request 수정할 값
+	 * @param adminId 수정할 관리자 아이디
+	 * @param session 검증을 위한 세션
+	 * @return 상태코드
+	 */
 	@PatchMapping("/{adminId}")
 	public ResponseEntity<Void> updateAdmin(@RequestBody UpdateAdminRequest request, @PathVariable Long adminId,
 		HttpSession session) {
@@ -80,6 +116,11 @@ public class AdminController {
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * 로그인 확인 메서드
+	 * @param session 검증을 위한 세션
+	 * @return 검증을 마친 세션의 세션값
+	 */
 	public AdminInfo checkSessionOrThrow(HttpSession session) {
 		AdminInfo adminInfo = (AdminInfo)session.getAttribute(AdminConst.ADMIN_INFO);
 		if (adminInfo == null) {
--- src/main/java/com/ecommerce/admins/service/AdminService.java
+++ src/main/java/com/ecommerce/admins/service/AdminService.java
@@ -27,6 +27,10 @@ public class AdminService {
 	private final AdminRepository adminRepository;
 	private final PasswordEncoder passwordEncoder;
 
+	/**
+	 * 회원가입: 리퀘스트 값을 받아 비밀번호 암호화 이후 서버에 저장
+	 * @param request 이름, 메일, 비번, 전화번호, 역할
+	 */
 	@Transactional
 	public void save(@Valid CreateAdminRequest request) {
 		if (adminRepository.existsByEmail(request.getEmail())) {
@@ -37,54 +41,106 @@ public class AdminService {
 			request.getAdminRole()));
 	}
 
+	/**
+	 * 로그인: 메일과 비밀번호가 일치하는지, 상태값을 확인 후 로그인
+	 * @param request 이메일과 로그인
+	 * @return 세션값 저장 후 반환
+	 */
 	@Transactional(readOnly = true)
-	public AdminInfo login(@Valid LoginAdminRequest request, AdminInfo adminInfo) {
-		Admin admin = findByIdOrThrow(adminInfo.getAdminId());
+	public AdminInfo login(@Valid LoginAdminRequest request) {
+		Admin admin = adminRepository.findByEmail(request.getEmail())
+			.orElseThrow(() -> new IllegalArgumentException("존재하는 이메일을 찾을 수 없습니다,"));
 		if (!passwordEncoder.matches(request.getPassword(), admin.getPassword())) {
 			throw new IllegalArgumentException("메일과 비밀번호가 일치하지 않습니다.");
 		}
-		switch (admin.getStatus()) {
-			case INACTIVE -> throw new IllegalArgumentException("계정 비활성화됨");
-			case SUSPENDED -> throw new IllegalArgumentException("계정 정지됨");
-			case PENDING -> throw new IllegalArgumentException("계정 승인대기중");
-			case REJECTED -> throw new IllegalArgumentException("계정 신청 거부됨");
-		}
+		checkStatusOrThrow(admin);
 
 		return new AdminInfo(admin.getAdminId(), admin.getEmail(), admin.getRole());
 	}
 
+	/**
+	 * 전체 관리자 조회: 슈퍼관리자, 상태를 확인 후 전체 관리자 조회
+	 * @param keyword 검색할 키워드
+	 * @param role 검색할 역할
+	 * @param status 검색할 상태
+	 * @param pageable 페이지네이션 조건
+	 * @param adminInfo 검증을 위한 세션값
+	 * @return 전체 관리자 반환
+	 */
 	@Transactional(readOnly = true)
 	public Page<GetAdminResponse> getAdminList(String keyword, AdminRole role, AdminStatus status, Pageable pageable,
 		AdminInfo adminInfo) {
 		checkRoleOrThrow(adminInfo);
 
+		Admin admin = findByIdOrThrow(adminInfo.getAdminId());
+		checkStatusOrThrow(admin);
+
 		return adminRepository.findAllByCondition(keyword, role, status, pageable).map(GetAdminResponse::from);
 	}
 
+	/**
+	 * 특정 관리자 조회
+	 * @param adminId 특정 관리자 아이디
+	 * @param adminInfo 검증을 위한 세션값
+	 * @return 검증 이후 특정 관리자의 이름, 메일, 전화번호, 역할, 상태, 생성일, 수락일 반환
+	 */
 	@Transactional(readOnly = true)
 	public GetOneAdminResponse getOne(Long adminId, AdminInfo adminInfo) {
 		checkRoleOrThrow(adminInfo);
-		Admin admin = findByIdOrThrow(adminId);
 
-		return GetOneAdminResponse.from(admin);
+		Admin requester = findByIdOrThrow(adminInfo.getAdminId());
+		checkStatusOrThrow(requester);
+
+		Admin target = findByIdOrThrow(adminId);
+
+		return GetOneAdminResponse.from(target);
 	}
 
+	/**
+	 * 관리자 정보 수정
+	 * @param adminId 수정할 관리자 아이디
+	 * @param request 수정할 정보
+	 * @param adminInfo 검증을 위한 세션값
+	 */
 	@Transactional
 	public void update(Long adminId, UpdateAdminRequest request, AdminInfo adminInfo) {
 		checkRoleOrThrow(adminInfo);
 		Admin admin = findByIdOrThrow(adminId);
+		checkStatusOrThrow(admin);
 
 		admin.updateAdmin(request.getName(), request.getEmail(), request.getPhoneNumber());
 	}
 
+	/**
+	 * 슈퍼 관리자 외 접근 불가 메서드
+	 * @param adminInfo 검증을 위한 세션
+	 */
 	public void checkRoleOrThrow(AdminInfo adminInfo) {
 		if (!adminInfo.getRole().equals(AdminRole.SUPER_ADMIN)) {
 			throw new IllegalArgumentException("권한이 없습니다");
 		}
 	}
 
+	/**
+	 * 아이디를 찾아 없을시 예외를 던지는 메서드
+	 * @param adminId 찾을 아이디
+	 * @return 예외처리를 마친 어드민 값
+	 */
 	public Admin findByIdOrThrow(Long adminId) {
 		return adminRepository.findById(adminId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+	}
+
+	/**
+	 * 활성화 외 접근 불가 메서드
+	 * @param admin 검증을 위한 어드민 값
+	 */
+	public void checkStatusOrThrow(Admin admin) {
+		switch (admin.getStatus()) {
+			case INACTIVE -> throw new IllegalArgumentException("계정 비활성화됨");
+			case SUSPENDED -> throw new IllegalArgumentException("계정 정지됨");
+			case PENDING -> throw new IllegalArgumentException("계정 승인대기중");
+			case REJECTED -> throw new IllegalArgumentException("계정 신청 거부됨");
+		}
 	}
 
 }
