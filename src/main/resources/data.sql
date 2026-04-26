9a80dd870936f94ab2aaf11c765521870031a2d1
HS <ehehqp@gmail.com>
2026-04-26 20:22:48 +0900

    fix : 초기 데이터 관리자 비밀번호 해시값으로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO admins (name, email, password, phone_number, role, status, deleted, created_at, modified_at)
-VALUES ('관리자', 'admin@test.com', '12345678', '010-9999-9999', 'SUPER_ADMIN', 'ACTIVE', false, NOW(), NOW());
+VALUES ('관리자', 'admin@test.com', '$2a$10$D65zesWQWcOekANz1GW0OeZ/PH3CRK/vhb3tDUrCsDSoEFSfw8z/m', '010-9999-9999', 'SUPER_ADMIN', 'ACTIVE', false, NOW(), NOW());
 
 INSERT INTO users (name, email, phone_number, status, deleted, created_at, modified_at)
 VALUES ('홍길동', 'user1@test.com', '010-1111-1111', 'ACTIVE', false, NOW(), NOW());
