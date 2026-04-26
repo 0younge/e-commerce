c4b25a842981083023fd11286b25a6e322daadcf
hjh <hjh690@naver.com>
2026-04-26 20:22:07 +0900

    feat: GetOneUserResponse에 orderCount, totalPrice 필드 추가

--- src/main/java/com/ecommerce/users/dto/GetOneUserResponse.java
+++ src/main/java/com/ecommerce/users/dto/GetOneUserResponse.java
@@ -1,8 +1,10 @@
 package com.ecommerce.users.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.ecommerce.common.enums.UserStatus;
+import com.ecommerce.orders.entity.Order;
 import com.ecommerce.users.entity.User;
 
 import lombok.Getter;
@@ -13,23 +15,31 @@ public class GetOneUserResponse {
 	private final String email;
 	private final String phoneNumber;
 	private final UserStatus status;
+	private final Long orderCount;
+	private final Long totalPrice;
 	private final LocalDateTime createdAt;
 
-	private GetOneUserResponse(String name, String email, String phoneNumber, UserStatus status,
+	private GetOneUserResponse(String name, String email, String phoneNumber, UserStatus status, Long orderCount,
+		Long totalPrice,
 		LocalDateTime createdAt) {
 		this.name = name;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 		this.status = status;
+		this.orderCount = orderCount;
+		this.totalPrice = totalPrice;
 		this.createdAt = createdAt;
 	}
 
 	public static GetOneUserResponse from(User user) {
+		List<Order> orders = user.getOrders();
 		return new GetOneUserResponse(
 			user.getName(),
 			user.getEmail(),
 			user.getPhoneNumber(),
 			user.getStatus(),
+			(long)orders.size(),
+			orders.stream().mapToLong(Order::getTotalPrice).sum(),
 			user.getCreatedAt()
 		);
 	}
