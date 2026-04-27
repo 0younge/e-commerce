9af0bac17543f03dce838a63943cdfc50a271ee2
hjh <hjh690@naver.com>
2026-04-27 14:21:44 +0900

    feat: 리뷰 단건 조회 서비스 로직 추가 리뷰 삭제 서비스 로직 추가

--- src/main/java/com/ecommerce/review/service/ReviewService.java
+++ src/main/java/com/ecommerce/review/service/ReviewService.java
@@ -2,12 +2,16 @@ package com.ecommerce.review.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.orders.entity.Order;
 import com.ecommerce.products.entity.Product;
+import com.ecommerce.review.dto.GetOneReviewResponse;
 import com.ecommerce.review.dto.GetReviewListResponse;
+import com.ecommerce.review.entity.Review;
 import com.ecommerce.review.repository.ReviewRepository;
 import com.ecommerce.users.entity.User;
 
@@ -35,5 +39,19 @@ public class ReviewService {
 					review.getCreatedAt()
 				);
 			});
+	}
+
+	@Transactional(readOnly = true)
+	public GetOneReviewResponse findById(Long reviewId) {
+
+		Review review = reviewRepository.findById(reviewId)
+			.orElseThrow(() -> new IllegalStateException("리뷰를 찾을 수 없습니다."));
+		return GetOneReviewResponse.from(review);
+	}
+
+	@Transactional
+	public void deleteById(Long reviewId) {
+		Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalStateException("리뷰를 찾을 수 없습니다."));
+		review.softDelete();
 	}
 }
