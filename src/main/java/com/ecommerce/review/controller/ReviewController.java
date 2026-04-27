1eae9f9a54642436d8c7a7c432fa9904d9f994d9
hjh <hjh690@naver.com>
2026-04-27 13:20:38 +0900

    feat: 리뷰 목록 조회 Controller 구현

---
+++ src/main/java/com/ecommerce/review/controller/ReviewController.java
@@ -0,0 +1,38 @@
+package com.ecommerce.review.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.common.response.ApiResponse;
+import com.ecommerce.review.dto.GetPageResponse;
+import com.ecommerce.review.dto.GetReviewListResponse;
+import com.ecommerce.review.service.ReviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+	private final ReviewService reviewService;
+
+	@GetMapping("/reviews")
+	public ResponseEntity<ApiResponse<GetPageResponse<GetReviewListResponse>>> getReviewList(
+		@RequestParam(required = false) String keyword,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(required = false, defaultValue = "createdAt") String sortBy,
+		@RequestParam(required = false, defaultValue = "ASC") String sortOrder,
+		@RequestParam(required = false) Integer rating) {
+
+		Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.valueOf(sortOrder), sortBy);
+		Page<GetReviewListResponse> result = reviewService.findByKeywordAndRating(keyword, rating, pageable);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(GetPageResponse.of(result)));
+	}
+}
