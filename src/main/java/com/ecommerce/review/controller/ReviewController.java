cc9bcabb79282d8ea8ca9d6f1b9e3cc4fd2ef82e
hjh <hjh690@naver.com>
2026-04-27 14:20:19 +0900

    feat: 리뷰 단건 조회 API 추가 리뷰 삭제 API 추가

--- src/main/java/com/ecommerce/review/controller/ReviewController.java
+++ src/main/java/com/ecommerce/review/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +35,21 @@ public class ReviewController {
 		Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.valueOf(sortOrder), sortBy);
 		Page<GetReviewListResponse> result = reviewService.findByKeywordAndRating(keyword, rating, pageable);
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(GetPageResponse.of(result)));
+	}
+
+	@GetMapping("/reviews/{reviewId}")
+	public ResponseEntity<ApiResponse<GetOneReviewResponse>> getOneReview(@PathVariable Long reviewId) {
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(reviewService.findById(reviewId)));
+	}
+
+	@DeleteMapping("/reviews/{reviewId}")
+	public ResponseEntity<ApiResponse<Void>> deleteReview(@PathVariable Long reviewId,
+		@SessionAttribute(name = AdminConst.ADMIN_INFO, required = false) AdminInfo adminInfo) {
+
+		if (adminInfo == null) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "관리자 로그인이 필요합니다.");
+		}
+		reviewService.deleteById(reviewId);
+		return ResponseEntity.ok(ApiResponse.success("리뷰가 삭제되었습니다."));
 	}
 }
