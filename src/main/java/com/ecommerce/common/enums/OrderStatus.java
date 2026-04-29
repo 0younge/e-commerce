67d9a3f671106e26a54aa432cbcb4a873bf85eef
jihyeon <bjh20049@naver.com>
2026-04-29 17:59:23 +0900

    refactor: OrderStatus valueOf 처리

--- src/main/java/com/ecommerce/common/enums/OrderStatus.java
+++ src/main/java/com/ecommerce/common/enums/OrderStatus.java
@@ -1,5 +1,10 @@
 package com.ecommerce.common.enums;
 
+import java.util.Arrays;
+
+import com.ecommerce.common.exception.InvalidRequestException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 /**
  * 주문 상태: 준비중, 배송중, 배송완료, 취소됨
  */
@@ -7,5 +12,13 @@ public enum OrderStatus {
 	READY,
 	SHIPPING,
 	DELIVERED,
-	CANCELED
+	CANCELED;
+
+	@JsonCreator
+	public static OrderStatus from(String value) {
+		return Arrays.stream(values())
+			.filter(v -> v.name().equalsIgnoreCase(value))
+			.findFirst()
+			.orElseThrow(() -> new InvalidRequestException("잘못된 상태값입니다."));
+	}
 }
