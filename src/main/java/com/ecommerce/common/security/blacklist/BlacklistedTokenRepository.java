542f157acefcb17112f415bc03082af9ffed0d1a
HS <ehehqp@gmail.com>
2026-04-29 11:28:10 +0900

    feat : 레포지토리 주석 추가

--- src/main/java/com/ecommerce/common/security/blacklist/BlacklistedTokenRepository.java
+++ src/main/java/com/ecommerce/common/security/blacklist/BlacklistedTokenRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
 
+	// 요청마다 이 토큰이 블랙리스트인지 확인
 	boolean existsByToken(String token);
 
+	// 만료된 토큰 삭제
 	void deleteByExpiredAtBefore(LocalDateTime now);
 }
