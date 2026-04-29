29ef2b346ca1bbb52c6d26995da6f8889be59499
HS <ehehqp@gmail.com>
2026-04-29 12:23:27 +0900

    fix :  로그아웃한 토큰으로 인증이 필요한 API에 접근하면 `401 Unauthorized`는 반환되지만 응답 메시지가 출력되지 않아 수정 기존 : 필터는 필터에서 직접 401 상태코드를 반환 후 종료 -> 변경 : 필터에서 직접 기존 구현해놓은 예외처리핸들러를 호출하도록 수정

--- src/main/java/com/ecommerce/common/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ecommerce/common/security/jwt/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.ecommerce.common.security.jwt;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.ecommerce.admins.entity.AdminRole;
 import com.ecommerce.common.security.auth.SecurityAdminInfo;
 import com.ecommerce.common.security.blacklist.TokenBlacklistService;
+import com.ecommerce.common.security.handler.CustomAuthenticationEntryPoint;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -25,6 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtTokenProvider jwtTokenProvider;
 	private final TokenBlacklistService tokenBlacklistService;
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
 	@Override
 	protected void doFilterInternal(
@@ -39,7 +42,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 			// 토큰이 블랙리스트에 존재하는지 검증
 			if (tokenBlacklistService.isBlacklisted(token)) {
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				customAuthenticationEntryPoint.commence(
+					request,
+					response,
+					new BadCredentialsException("로그아웃된 토큰입니다.")
+				);
 				return;
 			}
 
