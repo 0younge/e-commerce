84accf86f562cd1a5e506c00b802c670ece83f42
HS <ehehqp@gmail.com>
2026-04-29 12:06:40 +0900

    fix : 주석 수정

--- src/main/java/com/ecommerce/common/security/blacklist/BlacklistCleanupScheduler.java
+++ src/main/java/com/ecommerce/common/security/blacklist/BlacklistCleanupScheduler.java
@@ -13,9 +13,11 @@ import lombok.RequiredArgsConstructor;
 public class BlacklistCleanupScheduler {
 	private final BlacklistedTokenRepository blacklistedTokenRepository;
 
+	// 매 정각마다 스켈줄링
 	@Scheduled(cron = "0 0 * * * *")
 	@Transactional
 	public void deleteExpiredTokens() {
+		// 현재 시간보다 이전에 만료된 토큰 전부 삭제
 		blacklistedTokenRepository.deleteByExpiredAtBefore(LocalDateTime.now());
 	}
 }
